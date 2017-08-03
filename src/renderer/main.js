a3cd70e6723368b888537b60bd5b3fbda4f0c225
Enrique Benitez <enriquebenitezjr@gmail.com>
2017-08-02 22:33:01 -0500

    Select and display images

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,1 +1,19 @@
-window.addEventListener('load', () => {})
+window.addEventListener('load', () => {
+  addImagesEvent()
+})
+
+function addImagesEvent () {
+  const thumbs = document.querySelectorAll('li.list-group-item')
+
+  thumbs.forEach(thumb => {
+    thumb.addEventListener('click', function () {
+      changeImage(this)
+    })
+  })
+}
+
+function changeImage (node) {
+  document.querySelector('li.selected').classList.remove('selected')
+  node.classList.add('selected')
+  document.getElementById('image-displayed').src = node.querySelector('img').src
+}
